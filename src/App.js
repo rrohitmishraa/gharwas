0b3edef34cf267884eef7df04eaa98a35a824973
rrohitmishraa <rohit255141@gmail.com>
2024-08-07 01:35:59 +0530

    not complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from "react";
+import Lottie from "lottie-react";
+import "./App.css";
+import rangoli from "./rangoli.json";
+
+const OptimizedLottie = memo(() => (
+  <Lottie
+    className="rangoli-anim"
+    animationData={rangoli}
+    background="transparent"
+    speed="0.5"
+    autoplay
+  />
+));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <OptimizedLottie />
+      <div className="content">
+        <img src="./components/paneshji.png" className="ganeshji-pic" alt="" />
+        <span className="text-two">
+          Harish Chandra Kumar Mishra <br /> & Kavita Mishra
+        </span>
+        <span className="top-text"> Cordially invite you to the </span>
+        <span className="gp"> Griha Pravesh Ceremony </span>
+        <span className="text-two"> OF OUR NEW HOME ON </span>
+        <span className="date"> 8th Nov 2024</span>
+        <span className="text-two"> 10:00AM ONWARDS </span>
+
+        <img src="./components/family.png" className="fam-pic" alt="" />
+
+        <span className="text-two">Akhtiyarpur, Samastipur, Bihar, 848127</span>
+      </div>
+      <div className="map">rohit</div>
     </div>
   );
 }
